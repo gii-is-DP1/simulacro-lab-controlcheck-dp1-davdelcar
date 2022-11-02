faa51b6ba93873dfbedf8b26e286a5fa4ee469d4
David del Hoyo Carcaboso <davdelcar@alum.us.es>
2022-11-02 22:42:28 +0100

    Entrega

--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -1,11 +1,27 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class ProductType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    @Column(unique = true)
+    @Size(min = 3, max = 15)
     String name;
 }
